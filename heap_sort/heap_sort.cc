05bbe4c5f1829827cb04005f8ff7ac9818fca5e3
Iouri Gordon <iouri.gordon@gmail.com>
2018-09-20 00:10:22 -0700

    heapify implementation, not tested

--- heap_sort/heap_sort.cc
+++ heap_sort/heap_sort.cc
@@ -9,12 +9,32 @@ using namespace std;
 #define MAX_ARRAY_ELEM 100
 #define MAX_ITERATIONS 10
 
-#define PARENT(i) floor(i-1)/2
-#define LEFT_CHILD i*2+1
-#define RIGHT_CHILD i*2+2
+#define PARENT(i) i
+#define LEFT_CHILD(i) i*2
+#define RIGHT_CHILD(i) i*2+1
 
 void heapify(vector<int>& to_sort)
 {
+    int temp,swap;
+    int left_chld;
+    int right_chld;
+    int prnt_node = PARENT(to_sort.size()-1);
+
+    for (;prnt_node>=0;) {
+        while (LEFT_CHILD(prnt_node) < to_sort.size()) {
+            if (to_sort[prnt_node] < to_sort[LEFT_CHILD(prnt_node)])
+                swap = LEFT_CHILD(prnt_node);
+            if ((RIGHT_CHILD(prnt_node) < to_sort.size()) && 
+                (to_sort[swap] < to_sort[RIGHT_CHILD(prnt_node)]))
+                swap = RIGHT_CHILD(prnt_node); 
+            if (swap!=prnt_node){
+                temp = to_sort[prnt_node];
+                to_sort[prnt_node] = to_sort[swap];
+                to_sort[swap] = temp;
+            }
+            prnt_node = swap;
+        }
+    }
 }
 
 int main(int argc, char* argv[])
