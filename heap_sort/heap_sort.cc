21f5bc4ebc75e92f96019b054cccca60fce6f2c0
Iouri Gordon <iouri.gordon@gmail.com>
2018-09-21 00:53:18 -0700

    implemented heap sort, need to print out heap as a tree

--- heap_sort/heap_sort.cc
+++ heap_sort/heap_sort.cc
@@ -9,20 +9,21 @@ using namespace std;
 #define MAX_ARRAY_ELEM 100
 #define MAX_ITERATIONS 10
 
-#define PARENT(i) i
-#define LEFT_CHILD(i) i*2
-#define RIGHT_CHILD(i) i*2+1
+#define PARENT(i) (i)/2
+#define LEFT_CHILD(i) i*2+1
+#define RIGHT_CHILD(i) i*2+2
 
 void heapify(vector<int>& to_sort)
 {
-    int temp,swap;
-    int left_chld;
-    int right_chld;
+    int heap_root,temp,swap;
     int prnt_node = PARENT(to_sort.size()-1);
 
-    for (;prnt_node>=0;) {
+    heap_root = prnt_node;
+    for (;heap_root>=0;) {
+        prnt_node = heap_root;
+        swap = prnt_node;
         while (LEFT_CHILD(prnt_node) < to_sort.size()) {
-            if (to_sort[prnt_node] < to_sort[LEFT_CHILD(prnt_node)])
+            if (to_sort[prnt_node] < to_sort[LEFT_CHILD(prnt_node)]) 
                 swap = LEFT_CHILD(prnt_node);
             if ((RIGHT_CHILD(prnt_node) < to_sort.size()) && 
                 (to_sort[swap] < to_sort[RIGHT_CHILD(prnt_node)]))
@@ -31,9 +32,12 @@ void heapify(vector<int>& to_sort)
                 temp = to_sort[prnt_node];
                 to_sort[prnt_node] = to_sort[swap];
                 to_sort[swap] = temp;
+            } else {
+                break;
             }
             prnt_node = swap;
         }
+        heap_root = heap_root - 1;
     }
 }
 
@@ -45,6 +49,8 @@ int main(int argc, char* argv[])
         cout << "+++++++++++++++++++++++" << endl;
     	generate_int_array(MAX_ARRAY_SIZE, MAX_ARRAY_ELEM, int_vector);
 	    print_int_array(int_vector);
+
+        heapify(int_vector);
 
         print_int_array(int_vector);
 
