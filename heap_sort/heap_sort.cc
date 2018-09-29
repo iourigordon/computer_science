eb65465071ac86ac49a23dc7c9f993c95ea143d9
Iouri Gordon <iouri.gordon@gmail.com>
2018-09-28 19:01:49 -0700

    very nice print

--- heap_sort/heap_sort.cc
+++ heap_sort/heap_sort.cc
@@ -15,19 +15,47 @@ using namespace std;
 #define MAX_ITERATIONS 10
 
 #define PARENT(i) (i)/2
-#define LEFT_CHILD(i) i*2+1
-#define RIGHT_CHILD(i) i*2+2
+#define LEFT_CHILD(i) (i)*2+1
+#define RIGHT_CHILD(i) (i)*2+2
 
 #define PRNT_BUFF_LENGTH 1024
 
-void print_heap(vector<int>& heap) 
+enum {
+    PRNT_LEFT_EDGE,
+    PRNT_RIGHT_EDGE,
+    PRNT_BOTH
+};
+
+int print_node_edges(char* buff, int offset, int type )
+{
+    switch (type) {
+        case PRNT_LEFT_EDGE:
+            sprintf(buff+offset-offset/4,"/");
+            buff[offset-offset/4+1] = ' ';
+            return offset-offset/4+1;
+        case PRNT_RIGHT_EDGE:
+            sprintf(buff+offset+2+offset/4,"\\");
+            buff[offset+2+offset/4+1] = ' ';
+            return offset+2+offset/4+1; 
+        case PRNT_BOTH:
+            sprintf(buff+offset-offset/4,"/");
+            buff[offset-offset/4+1] = ' ';
+            sprintf(buff+offset+2+offset/4,"\\");
+            buff[offset+2+offset/4+1] = ' ';
+            return offset+2+offset/4+1; 
+    }
+    return 0;
+}
+
+void print_heap(vector<int>& heap, int node_width) 
 {
     int root;
     int curr_line, next_line;
     char prnt_buff[PRNT_BUFF_LENGTH];
+    char edge_buff[PRNT_BUFF_LENGTH];
     int heap_height = pow(2,floor(log2(heap.size())))*4;
     int prnt_offset = heap_height;
-    int chld_delta = (prnt_offset-2)/2-1;
+    int chld_delta = (prnt_offset-node_width)/2-1;
     queue<int> heap_queue;
 
     cout << "heap.size() = " << heap.size() << endl;
@@ -40,13 +68,17 @@ void print_heap(vector<int>& heap)
         sprintf(prnt_buff+i," ");
     cout << prnt_buff;
     cout << heap[root] << endl;
+    memset(edge_buff,' ',PRNT_BUFF_LENGTH);
+    edge_buff[print_node_edges(edge_buff,chld_delta,PRNT_BOTH)+1] = '\0'; 
+    cout << edge_buff << endl;
+
     curr_line = 1;
     next_line = 0;
     heap_queue.push(root);
     
-
+    memset(edge_buff,' ',PRNT_BUFF_LENGTH);
     memset(prnt_buff,0,PRNT_BUFF_LENGTH);
-    for (int i=0;i<(chld_delta-2)/2;i++)
+    for (int i=0;i<(chld_delta-node_width)/2;i++)
         sprintf(prnt_buff+i," ");
     cout << prnt_buff;
  
@@ -55,6 +87,9 @@ void print_heap(vector<int>& heap)
         heap_queue.pop();
         if (LEFT_CHILD(root) < heap.size()) {
             cout << heap[LEFT_CHILD(root)];
+            if (LEFT_CHILD(LEFT_CHILD(root)) < heap.size()) {
+                print_node_edges(edge_buff,(chld_delta-node_width)/2,PRNT_LEFT_EDGE);
+            }
             heap_queue.push(LEFT_CHILD(root));
             next_line+=1;
         }
@@ -70,11 +105,14 @@ void print_heap(vector<int>& heap)
         }
         if (--curr_line == 0) {
             cout << endl;
+            edge_buff[heap_height] = '\0';
+            cout << edge_buff << endl;
             curr_line = next_line;
             next_line = 0;
-            chld_delta = (chld_delta-2)/2;
+            chld_delta = (chld_delta-node_width)/2;
             memset(prnt_buff,0,PRNT_BUFF_LENGTH);
-            for (int i = 0; i < (chld_delta-2)/2;i++)
+            memset(edge_buff,' ',PRNT_BUFF_LENGTH);
+            for (int i = 0; i < (chld_delta-node_width)/2;i++)
                 sprintf(prnt_buff+i," ");
             cout << prnt_buff;
         } else {
@@ -128,7 +166,7 @@ int main(int argc, char* argv[])
 
         print_int_array(int_vector);
 
-        print_heap(int_vector);
+        print_heap(int_vector,2);
         int_vector.erase(int_vector.begin());
         cout << "-----------------------" << endl;
     }
